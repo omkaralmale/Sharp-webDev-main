323a3be83a6fb8bcb5944c725ec44d4a8ae0959c
omkaralmale <omkaralmale@gmail.com>
2023-11-02 21:23:22 +0530

    basics import and export concepts

--- Full-Stack/front-end development/first_create/src/App.js
+++ Full-Stack/front-end development/first_create/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return <h2>Hello, Let's Start</h2>;
+  return (
+    <>
+      <h2>Hello, Let's Start</h2>
+      <ExpenseItem></ExpenseItem>
+    </>
+  );
 }
 
 export default App;
---
+++ Full-Stack/front-end development/first_create/src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+  return (
+    <>
+      <h2>Expense_Item !</h2>
+      <h4>Food Rs 10</h4>
+      <h4>Petrol Rs 100</h4>
+      <h4>Movies Rs 200</h4>
+    </>
+  );
+}
+export default ExpenseItem;
