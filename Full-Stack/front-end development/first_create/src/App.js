2bfe69710996c4bc47822b75f3c2b59d72d157ef
omkaralmale <omkaralmale@gmail.com>
2023-11-02 23:02:16 +0530

    date props

--- Full-Stack/front-end development/first_create/src/App.js
+++ Full-Stack/front-end development/first_create/src/App.js
@@ -1,28 +1,28 @@
 import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 
-function App() {
-  const expenses = [
-    {
-      date: "2023-11-02",
-      name: "Food",
-      amount: 10,
-      location: "Restaurant",
-    },
-    {
-      date: "2023-11-03",
-      name: "Petrol",
-      amount: 100,
-      location: "Gas Station",
-    },
-    {
-      date: "2023-11-04",
-      name: "Movies",
-      amount: 200,
-      location: "Cinema",
-    },
-  ];
+const expenses = [
+  {
+    date: new Date("2023-11-02"),
+    name: "Food",
+    amount: 10,
+    location: "Restaurant",
+  },
+  {
+    date: new Date("2023-11-03"),
+    name: "Petrol",
+    amount: 100,
+    location: "Gas Station",
+  },
+  {
+    date: new Date("2023-11-04"),
+    name: "Movies",
+    amount: 200,
+    location: "Cinema",
+  },
+];
 
+function App() {
   return (
     <div>
       {expenses.map((expense, index) => (
