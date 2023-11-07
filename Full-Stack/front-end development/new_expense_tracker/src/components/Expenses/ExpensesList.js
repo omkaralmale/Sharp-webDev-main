a578f20f7204a6434004f6a38160e61c3b628145
omkaralmale <omkaralmale@gmail.com>
2023-11-07 22:35:29 +0530

    Done

--- Full-Stack/front-end development/new_expense_tracker/src/components/Expenses/ExpensesList.js
+++ Full-Stack/front-end development/new_expense_tracker/src/components/Expenses/ExpensesList.js
@@ -6,6 +6,14 @@ function ExpensesList(props) {
   if (props.items.length === 0) {
     return <h2 className="expenses-list__fallback"> No Expenses Found.</h2>;
   }
+  if (props.items.length === 1) {
+    return (
+      <h2 className="expenses-list__fallback">
+        {" "}
+        Only single Expense here. Please add more...
+      </h2>
+    );
+  }
   return (
     <ul className="expenses-list">
       {props.items.map((expense) => (
