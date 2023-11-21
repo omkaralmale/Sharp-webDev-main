2f657cd429bd8e7cea24888bc9eba055ba07e49c
omkaralmale <omkaralmale@gmail.com>
2023-11-21 15:07:58 +0530

    form added

--- Full-Stack/front-end development/food-site/src/components/Meals/MealItem/MealItem.js
+++ Full-Stack/front-end development/food-site/src/components/Meals/MealItem/MealItem.js
@@ -1,14 +1,19 @@
 import React from "react";
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
+
 const MealItem = (props) => {
   return (
     <li key={props.key} className={classes.meal}>
-      <div>
+      <div className="list-container">
         <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.rating}>⭐{props.rating}/5</div>
+        <div className={classes.price}>${props.price.toFixed(2)}</div>
       </div>
-      <div className={classes.description}>{props.description}</div>
-      <div className={classes.rating}>{props.rating}</div>
-      <div className={classes.price}>$ {props.price.toFixed(2)}</div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 };
---
+++ Full-Stack/front-end development/food-site/src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,24 @@
+import React from "react";
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
+
+const MealItemForm = () => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Qty:"
+        input={{
+          id: "amount",
+          type: "number",
+          min: 1,
+          max: 5,
+          step: 1,
+          defaultValue: 1,
+        }}
+      />
+      <button>+ Add Dish</button>
+    </form>
+  );
+};
+
+export default MealItemForm;
